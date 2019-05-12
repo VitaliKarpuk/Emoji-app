bbfa928e3804e1352d5262f20ac9299468c89eb2
VitaliKarpuk <karpuk-job@yandex.ru>
2019-05-12 19:47:45 +0300

    Homework 2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {NameForm} from './Components/NameForm';
+import './style.css'
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root'),
+);
